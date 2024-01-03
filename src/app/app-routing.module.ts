8160eda4aa3f5597c4cf16a21311f0953bb8a9d3
Viktor Daniel Varga <vvd007@gmail.com>
2024-01-03 03:03:34 +0100

    Protecting training path with auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,16 +5,18 @@ import { WelcomeComponent } from "./welcome/welcome.component";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { TrainingComponent } from "./training/training.component";
 import { LoginComponent } from "./auth/login/login.component";
+import { AuthGuard } from "./auth/auth.guard";
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'training', component: TrainingComponent },
+  { path: 'training', component: TrainingComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  exports: [ RouterModule ],
+  providers: [ AuthGuard ] // normally this should be in the module, but we use AuthGuard *only* in this module
 })
 export class AppRoutingModule {}
